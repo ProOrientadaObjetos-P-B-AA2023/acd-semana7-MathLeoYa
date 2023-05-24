30d93a865ed245373835923c4ebac24726b6e4a7
MathLeoYa <mateoleonardo1616@gmail.com>
2023-05-24 16:15:24 -0500

    semana7

---
+++ EjemploPOOArchivosTextoSecuencial/ArchivosSecuencialPOO/src/Utilerias/ProbandoArchivos.java
@@ -0,0 +1,12 @@
+
+package Utilerias;
+import java.io.FileNotFoundException;
+import java.io;
+import java.util.Formatter;
+
+public class ProbandoArchivos {
+    public static void main(String[] args) throws FileNotFoundException{
+        Formatter archivoSalida=new Formatter("Miprueba.txt");  
+        
+    }
+}
